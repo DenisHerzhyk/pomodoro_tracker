8327cfd0cb1ecc323d34a32750ecfeec676ded68
DenisHerzhyk <denis.herzhyk88@gmail.com>
2026-06-21 22:08:49 +0300

    Fixed task creation, update, delete, search requests

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,5 @@
 # run server : uvicorn src.main:app --reload
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import select
 from .dependency import SessionDep
 from .models import Task
@@ -16,17 +16,44 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers={"*"}
 )
+
 #create table
 @app.on_event("startup")
 async def on_startup():
     return create_db_and_tables()
 
 
-#read task
-@app.get("/api/py/task")
-async def read_tasks(session: SessionDep) -> List[Task]:
-    tasks = session.exec(select(Task)).all()
+#read incomplete task
+@app.get("/api/py/incomplete_tasks")
+async def read_incomplete_tasks(session: SessionDep) -> List[Task]:
+    tasks = session.exec(select(Task).where(Task.is_completed == False)).all()
     return tasks
+
+#read complete task 
+@app.get("/api/py/complete_tasks")
+async def read_complete_tasks(session:SessionDep) -> List[Task]:
+    tasks = session.exec(select(Task).where(Task.is_completed == True)).all()
+    return tasks
+
+#get task 
+@app.get("/api/py/task/{task_id}")
+async def find_task(task_id: int, session: SessionDep) -> Task:
+    task = session.get(Task, task_id)
+    if not task:
+        HTTPException(status_code=404, detail=f"Task '${task_id}' was not found")
+
+    return task
+
+#delete task
+@app.delete("/api/py/task/{task_id}")
+async def delete_task(task_id: int, session: SessionDep) -> dict[str, bool]:
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task '${task_id}' was not found")
+    
+    session.delete(task)
+    session.commit()
+    return {"ok": True}
 
 #create task
 @app.post("/api/py/task")
@@ -36,6 +63,20 @@ async def create_task(task: Task, session: SessionDep) -> Task:
     session.refresh(task)
     return task
 
+#toggle task
+@app.patch("/api/py/task/{task_id}")
+async def toggle_task(task_id: int, session: SessionDep) -> Task:
+    print("WE ARE IN PATCH")
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail=f"The task id '{task_id}' was not found")
+
+    task.is_completed = not task.is_completed
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    print("FINISHED THE PATCH")
+    return task
 
 # @app.post("/api/py/task/{task.id}")
 # async def create_task():
